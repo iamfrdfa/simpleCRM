7723e8d528646ccce60cf344af7298beb36a0226
iamfrdfa <fri.faraji@gmail.com>
2026-02-24 14:43:41 +0100

    added user detail site

--- src/app/user-detail/user-detail.ts
+++ src/app/user-detail/user-detail.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
+import {UI_IMPORTS} from '../shared/ui.imports';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-user-detail',
-  imports: [],
-  templateUrl: './user-detail.html',
-  styleUrl: './user-detail.scss'
+    selector: 'app-user-detail',
+    imports: [
+        UI_IMPORTS
+    ],
+    templateUrl: './user-detail.html',
+    styleUrl: './user-detail.scss'
 })
 export class UserDetail {
+    userId: string | null =  '';
 
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit() : void {
+        this.route.paramMap.subscribe(params => {
+            this.userId = params.get('id');
+            console.log("Got ID:", this.userId);
+        });
+    }
 }
